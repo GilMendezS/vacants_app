c647abc941663ff344cbfe303a00eab0c0b1520e
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-26 15:26:11 -0600

    Updating companies

--- api/controllers/company.js
+++ api/controllers/company.js
@@ -1,13 +1,33 @@
-const Comapny = require('../models/company');
+const Company = require('../models/company');
 exports.getCompanies = async (req, res, next) => {
     try {
-        const companies = await Comapny.find();
+        const companies = await Company.find();
         return res.status(200).json({
             data: companies
         })
     } catch (error) {
         return res.status(500).json({
             message: 'Error fetching companies',
+            error
+        })
+    }
+}
+exports.addCompany = async (req, res, next) => {
+    console.log(req.body)
+    const company = new Company({
+        title: req.body.name,
+        imageUrl: req.file ? req.file.path : '',
+        description : req.body.description
+    })
+    try {
+        await company.save();
+        return res.status(200).json({
+            message: 'Company saved!',
+            data: company
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error saving the company',
             error
         })
     }
--- api/routes/company.js
+++ api/routes/company.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const CompanyController = require('../controllers/company');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/companies/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+})
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg'){
+        cb(null, true)
+    }
+    else {
+        cb(new Error('Only .jpeg or .png files are accepted'), false)
+    }
+}
+const upload = multer({
+    storage,
+    limits: {
+        fileSize : 1024 * 1024 * 5
+    },
+    fileFilter
+})
 router.get('/', CompanyController.getCompanies);
 
+router.post('/', upload.single('companyImage'), CompanyController.addCompany)
 module.exports = router;
