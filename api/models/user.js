4a3989cb9fe60a8d33d1b5eee1e821d5f087ba4d
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-26 12:30:10 -0600

    :clipboard: user model

---
+++ api/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    lastname : {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phones: [
+        {
+            type: String,
+        }
+    ],
+    lookingFor: {
+        type: Boolean,
+        default: true,
+    },
+    cv: [
+        {
+            type: String,
+        }
+    ],
+    birthdate : {
+        type: Date
+    },
+    description: {
+        type: String
+    }
+});
+userSchema.pre('save', function(){
+    bcrypt.hash(this.password, 10, (err, hashedPAssword) => {
+        if(!err){
+            this.password = hashedPAssword;
+        }
+    })
+    next();
+})
+module.exports = mongoose.model('User', userSchema);
