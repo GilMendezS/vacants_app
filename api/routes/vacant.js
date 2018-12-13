f4b722a2ed2d8b80a54007855ed0048bc66a68df
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-13 14:57:16 -0600

    :clipboard: new endpoints
    vacants module

---
+++ api/routes/vacant.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const VacantController = require('../controllers/vacant');
+const middleware = require('../middlewares/verify_token');
+
+
+router.get('/',VacantController.getActiveVacants);
+
+router.get('/all', [middleware.check_token],VacantController.getAllVacants);
+
+router.get('/:id', VacantController.getVacant);
+
+router.post('/', [middleware.check_token],VacantController.addVacant);
+
+router.post('/:id/apply', [middleware.check_token], VacantController.apply);
+
+router.put('/:id', [middleware.check_token],VacantController.updateVacant);
+
+router.delete('/:id', [middleware.check_token], VacantController.removeVacant)
+
+module.exports = router;
--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const companyRoutes = require('./api/routes/company');
 const contractRoutes = require('./api/routes/contract');
 const statusRoutes = require('./api/routes/status');
 const userRoutes = require('./api/routes/user');
+const vacantRoutes = require('./api/routes/vacant');
 const port = process.env.PORT || 3000;
 app.use('/api/categories', categoryRoutes);
 app.use('/api/companies', companyRoutes);
 app.use('/api/contracts', contractRoutes)
 app.use('/api/statuses', statusRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/vacants', vacantRoutes)
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost:27017/vacants',{ useNewUrlParser: true })
 .then(db => {
