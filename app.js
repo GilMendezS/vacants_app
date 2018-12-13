bfe44b4a0712d300c165349f34191a7e536ba4b3
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-13 16:51:11 -0600

    cors enabled on api

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
+app.use(cors());
 
 const categoryRoutes = require('./api/routes/category');
 const companyRoutes = require('./api/routes/company');
