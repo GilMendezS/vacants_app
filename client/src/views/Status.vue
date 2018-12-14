ccc25c9d0e646aac7a3416ae84f4f8e89285fb29
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-14 13:37:12 -0600

    :clipboard: listing statuses and form to create a new status generetaed

--- client/src/views/Status.vue
+++ client/src/views/Status.vue
@@ -1,66 +1,76 @@
 <template>
-    <v-card>
-        <v-card-title>
-            <v-icon>
-                announcement
-            </v-icon>
-             Statuses
-            <v-spacer></v-spacer>
-            <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
-            ></v-text-field>
-        </v-card-title>
-        <v-data-table
-            :headers="headers"
-            :items="statuses"
-            :search="search"
-        >
-            <template slot="items" slot-scope="props">
-                <td class="text-xs-left">{{ props.item.title }}</td>
-                <td class="text-xs-left">
-                    <v-chip v-if="props.item.active" class="success">
-                        Yes
-                    </v-chip>
-                    <v-chip v-else class="error">
-                        No
-                    </v-chip>
-                </td>
-                <td class="justify-center layout px-0">
-                    <v-icon
-                        small
-                        class="mr-2 blue--text"
-                        @click="editStatus(props.item)"
+    <v-container>
+        <v-layout>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title>
+                        <v-icon>
+                            announcement
+                        </v-icon>
+                        Statuses
+                        <v-spacer></v-spacer>
+                        <v-text-field
+                        v-model="search"
+                        append-icon="search"
+                        label="Search"
+                        single-line
+                        hide-details
+                        ></v-text-field>
+                    </v-card-title>
+                    <v-data-table
+                        :headers="headers"
+                        :items="statuses"
+                        :search="search"
                     >
-                        edit
-                    </v-icon>
-                    <v-icon
-                        small
-                        class="red--text"
-                        @click="removeStatus(props.item)"
+                        <template slot="items" slot-scope="props">
+                            <td class="text-xs-left">{{ props.item.title }}</td>
+                            <td class="text-xs-left">
+                                <v-chip v-if="props.item.active" class="success">
+                                    Yes
+                                </v-chip>
+                                <v-chip v-else class="error">
+                                    No
+                                </v-chip>
+                            </td>
+                            <td class="justify-center layout px-0">
+                                <v-icon
+                                    small
+                                    class="mr-2 blue--text"
+                                    @click="editStatus(props.item)"
+                                >
+                                    edit
+                                </v-icon>
+                                <v-icon
+                                    small
+                                    class="red--text"
+                                    @click="removeStatus(props.item)"
+                                >
+                                    delete
+                                </v-icon>
+                            </td>
+                        </template>
+                        <v-alert slot="no-results" :value="true" color="error" icon="warning">
+                            Your search for "{{ search }}" found no results.
+                        </v-alert>
+                    </v-data-table>
+                    <v-add-status /> 
+                    <v-float-button
+                        :color="'blue'"
                     >
-                        delete
-                    </v-icon>
-                </td>
-            </template>
-            <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                Your search for "{{ search }}" found no results.
-            </v-alert>
-        </v-data-table>
-        <v-float-button
-            :color="'blue'"
-        >
-        </v-float-button>
-    </v-card>
+                    </v-float-button>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+    
 </template>
 <script>
+import AddStatus from '../components/statuses/AddStatus.vue';
 import FloatButon from '../components/buttons/FloatButton.vue';
 export default {
     components: {
-        'v-float-button': FloatButon
+        'v-float-button': FloatButon,
+        'v-add-status': AddStatus
     },
     data(){
         return {
