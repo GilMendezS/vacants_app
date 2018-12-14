16adf96984209287185a0354d35d8dc0d1845506
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-13 18:23:47 -0600

    app.js modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const env = require('./api/env')
+const env = require('./api/env.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
