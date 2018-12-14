b81220f8fc0b4b2829034340b24606cb9f4b07eb
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-14 13:36:48 -0600

    :clipboard: message component modified

--- client/src/components/messages/Message.vue
+++ client/src/components/messages/Message.vue
@@ -1,28 +1,40 @@
 <template>
-    <v-container>
-        <v-layout>
-            <v-flex xs12>
+    <v-container v-if="displayingMessage">
+        <v-layout row>
+            <v-flex xs11>
                 <v-alert
-                    v-model="displayingMessage"
-                    transition="scale-transition"
-                    :type="type"
-                    outline
-                    dismissible
+                :value="displayingMessage"
+                :type="type"
+                transition="scale-transition"
                 >
-                    {{message}}                    
+                    {{message}}
+                    
                 </v-alert>
+                
+            </v-flex>
+            <v-flex xs1>
+                <v-icon @click="closeAlertMessage" right="right" color="red">
+                    cancel
+                </v-icon>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 <script>
 export default {
+    data(){
+        return {
+            timeout: 3000,
+            show: false,
+            right: true
+        }
+    },
     computed: {
         displayingMessage(){
             return this.$store.getters['message/getStateMessage'];
         },
         type(){
-            return this.$store.getters['message/getType'];
+            return this.$store.getters['message/getTypeMessage'];
         },
         message(){
             return this.$store.getters['message/getMessage']
@@ -35,4 +47,10 @@ export default {
     }
 }
 </script>
+<style scoped>
+    .float-right{
+        float: right !important;
+        position: absolute;
+    }
+</style>
 
--- client/src/store/status.js
+++ client/src/store/status.js
@@ -7,6 +7,9 @@ export default {
     mutations: {
         setLoadedStatuses(state, payload){
             state.statuses = payload;
+        },
+        setANewStatus(state, payload){
+            state.statuses = [...state.statuses, payload]
         }
     },
     actions: {
@@ -15,6 +18,22 @@ export default {
             .then(response => {
                 console.log(response.data);
                 commit('setLoadedStatuses', response.data.data);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        addStatus({dispatch,commit, rootGetters}, payload){
+            axios.post(`${rootGetters.url_api}/statuses`, payload)
+            .then(response => {
+                console.log(response.data)
+                if(response.status === 200){
+                    commit('setANewStatus', response.data.data);
+                    dispatch('message/addMessage', {
+                        message: response.data.message,
+                        type: 'success'
+                    }, { root: true });
+                }
             })
             .catch(err => {
                 console.log(err)
