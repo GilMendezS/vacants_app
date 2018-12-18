4a4fb1811755101df2ac373eb07b66e93f7bb268
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-18 17:35:52 -0600

    :clipboard: file removed

---
+++ api/env.js
@@ -0,0 +1,6 @@
+const credentials = {
+    jwt_private: 'GMSprivatekey',
+    db: 'mongodb://user_app:IbPoiwKcrkHkPZ3x@cluster0-shard-00-00-fpn8d.mongodb.net:27017,cluster0-shard-00-01-fpn8d.mongodb.net:27017,cluster0-shard-00-02-fpn8d.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+}   
+module.exports = credentials
+
